902315aeef1829cf9eafc7de25c7d2f3c5e105fe
cghil <cghilarducci@gmail.com>
2015-06-08 16:13:49 -0700

    keypress for enter, key arrows now work

--- assets/custom/javascript/main.js
+++ assets/custom/javascript/main.js
@@ -3,28 +3,67 @@ $(document).ready(function(){
 	var $input = $('#contact-form');
 	var $contactsBox = $("#possible-contacts");
 	var contactTemplate = _.template($('#contact-template').html());
-
+	var notFoundTemplate = _.template($('#not-found-template').html());
+	var downarrowCounter = 0
 	hideBox();
 	$input.keyup(showData);
+	$input.keypress(checkForSelected);
+
+	function checkForSelected(e){
+		var keyvalue = e.which
+		if (keyvalue === 13){
+			var $contactSelected = $('.selected-contact');
+			if ($contactSelected.length === 1){
+				var email = $contactSelected.find('.email').text();
+				removeParentheses(email);
+			}
+		}
+	}
 
 	function showData(e){
-		emptyBox();
-		showBox();
-		var text = $(this).val();
-		var regex = createRegexObj("^"+text+"");
-		var results = filter(EMAIL_DATA, regex);
-		for (var index=0; index < results.length; index++){
-			var newContactGuess = $contactsBox.append(contactTemplate({
-				name: results[index].name,
-				email: results[index].address
-			}));
-			if (index >= 3){ break; }
-		// 4 divs will be appended
-		// would have used forEach but forEach does not allow a loop break
+		var keyvalue = e.which
+		if (keyvalue !== 13){
+			if (keyvalue !== 40 && keyvalue !== 38) {
+				downarrowCounter = 0
+			}
+			emptyBox();
+			showBox();
+			var text = $(this).val();
+			var regex = createRegexObj("^"+text+"");
+			var results = filter(EMAIL_DATA, regex);
+			if (results.length === 0){
+				$contactsBox.append(notFoundTemplate());
+			}
+			for (var index=0; index < results.length; index++){
+				var newContactGuess = $contactsBox.append(contactTemplate({
+					name: results[index].name,
+					email: results[index].address
+				}));
+				if (index >= 3){ break; }
+			// 4 divs will be appended
+			// would have used forEach but forEach does not allow a loop break
+			}
+			if (keyvalue === 40){
+				$contactContainers = $('.single-contact')
+				if (downarrowCounter > 0) {
+					$($contactContainers[downarrowCounter - 1]).removeClass('selected-contact');
+				}
+				$($contactContainers[downarrowCounter]).addClass('selected-contact');
+				if (downarrowCounter < 3){
+					downarrowCounter = downarrowCounter + 1;
+				}
+			}
+
+			if (keyvalue === 38){
+				$contactContainers = $('.single-contact')
+				if (downarrowCounter > 0 ) {
+					$($contactContainers[downarrowCounter]).removeClass('selected-contact');
+					downarrowCounter = downarrowCounter -1
+					$($contactContainers[downarrowCounter]).addClass('selected-contact');
+				}
+			}
 		}
-		// if (e.which === 40) {
-		// 	addSelectionViaEnter()
-		// }
+
 		addClickToContacts();
 		nothingInInputform(text);
 	}
@@ -34,22 +73,28 @@ $(document).ready(function(){
 		$contactContainers.each(function(){
 			$(this).click(function(){
 				var email = $(this).find('.email').text();
-				email = email.replace('(', '');
-				email = email.replace(')', '');
-				$input.val(email);
-				emptyBox();
-				hideBox();
+				removeParentheses(email);
 			})
 			$(this).mouseover(addSelected);
 			$(this).mouseout(removeSelected);
 		})
 	}
 
+	function removeParentheses(email){
+		email = email.replace('(', '');
+		email = email.replace(')', '');
+		$input.val(email);
+		emptyBox();
+		hideBox();
+	}
+
 	function removeSelected(){
+		downarrowCounter = 0;
 		$(this).removeClass('selected-contact');
 	}
 
 	function addSelected(){
+		$('.selected-contact').removeClass('selected-contact');
 		$(this).addClass('selected-contact');
 	}
 
